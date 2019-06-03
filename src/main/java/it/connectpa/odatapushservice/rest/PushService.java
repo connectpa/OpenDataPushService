af40ff7d5f5c7bf1acced7254f52ac566b422a71
dima.ayash <dima.ayash@tirasa.net>
2019-06-03 16:28:39 +0200

    change push servcie requests, respons + more test added

--- src/main/java/it/connectpa/odatapushservice/rest/PushService.java
+++ src/main/java/it/connectpa/odatapushservice/rest/PushService.java
@@ -1,21 +1,27 @@
 package it.connectpa.odatapushservice.rest;
 
+import com.opencsv.CSVReader;
 import it.connectpa.odatapushservice.server.api.ApiApi;
 import it.connectpa.odatapushservice.server.api.ResourceApi;
-import it.connectpa.odatapushservice.server.model.InlineObject;
-import it.connectpa.odatapushservice.server.model.InlineObject1;
-import it.connectpa.odatapushservice.server.model.InlineObject2;
-import it.connectpa.odatapushservice.server.model.InlineResponse201;
+import it.connectpa.odatapushservice.server.model.Column;
+import it.connectpa.odatapushservice.server.model.InstertedData;
+import it.connectpa.odatapushservice.server.model.CreatedColumn;
+import it.connectpa.odatapushservice.server.model.CreatedMetadata;
+import it.connectpa.odatapushservice.server.model.Metadata;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Optional;
-import javax.validation.Valid;
+import java.util.UUID;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 public class PushService implements ApiApi, ResourceApi {
@@ -28,26 +34,50 @@ public class PushService implements ApiApi, ResourceApi {
     }
 
     @Override
-    public ResponseEntity<Void> addType(
-            final @PathVariable("id") String id,
-            final @Valid @RequestBody InlineObject1 body) {
+    public ResponseEntity<CreatedMetadata> createMetadata(final @RequestBody Metadata metadata) {
+        LOG.info("POST for creating metadata: {}", metadata);
 
-        LOG.info("POST: {} {}", id, body);
-        return null;
-    }
+        CreatedMetadata responsePayload = new CreatedMetadata();
+        responsePayload.setId(UUID.randomUUID().toString());
+        responsePayload.setDescription(metadata.getDescription());
+        responsePayload.setName(metadata.getName());
 
-    @Override
-    public ResponseEntity<InlineResponse201> createMetadata(final InlineObject body) {
-        LOG.info("POST: {}", body);
-
-        InlineResponse201 responsePayload = new InlineResponse201();
-        responsePayload.setId("abcd-efgh");
         return new ResponseEntity<>(responsePayload, HttpStatus.CREATED);
     }
 
     @Override
-    public ResponseEntity<Void> insertData(final String id, final InlineObject2 body) {
-        LOG.info("PUT: {} {}", id, body);
-        return null;
+    public ResponseEntity<CreatedColumn> addColumn(
+            final @PathVariable("id") String id,
+            final @RequestBody Column column) {
+        LOG.info("POST to create a new column : {}  in a dataset with id {}", column, id);
+
+        CreatedColumn responsePayload = new CreatedColumn();
+        responsePayload.setId(id);
+        responsePayload.setDataTypeName(column.getDataTypeName());
+        responsePayload.setName(column.getName());
+
+        return new ResponseEntity<>(responsePayload, HttpStatus.CREATED);
+    }
+
+    @Override
+    public ResponseEntity<InstertedData> insertData(
+            final @PathVariable("id") String id,
+            final @RequestParam("file") MultipartFile file) {
+        LOG.info("PUT: {} {}", id, file.getOriginalFilename());
+
+        InputStreamReader reader;
+        InstertedData responsePayload = new InstertedData();
+        try {
+            reader = new InputStreamReader(file.getInputStream());
+            CSVReader csvReader = new CSVReader(reader);
+            Integer recordsNumber = csvReader.readAll().size() - 1;
+            responsePayload.setId(id);
+            responsePayload.setRecordsNumber(recordsNumber);
+            LOG.info("Number of records {}", recordsNumber);
+        } catch (IOException e) {
+            LOG.error("While parsing CSV file {}", e.getMessage());
+        }
+
+        return new ResponseEntity<>(responsePayload, HttpStatus.CREATED);
     }
 }
--- src/test/java/it/connectpa/odatapushservice/PushServiceTests.java
+++ src/test/java/it/connectpa/odatapushservice/PushServiceTests.java
@@ -4,9 +4,12 @@ import static org.junit.Assert.assertEquals;
 
 import it.connectpa.odatapushservice.client.ApiClient;
 import it.connectpa.odatapushservice.client.api.PushServiceApi;
-import it.connectpa.odatapushservice.client.model.InlineObject;
-import it.connectpa.odatapushservice.client.model.InlineObject1;
-import it.connectpa.odatapushservice.client.model.InlineResponse201;
+import it.connectpa.odatapushservice.client.model.Column;
+import it.connectpa.odatapushservice.client.model.CreatedColumn;
+import it.connectpa.odatapushservice.client.model.CreatedMetadata;
+import it.connectpa.odatapushservice.client.model.InstertedData;
+import it.connectpa.odatapushservice.client.model.Metadata;
+import java.io.File;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -23,17 +26,33 @@ public class PushServiceTests {
     private int port;
 
     @Test
-    public void addType() {
+    public void createMetadataTest() {
         PushServiceApi api = new PushServiceApi(new ApiClient().setBasePath("http://localhost:" + port));
-        InlineObject1 payload = new InlineObject1();
-        api.addType("abcd-efgh", payload);
+        Metadata payload = new Metadata();
+        payload.setName("TEST");
+        payload.setDescription("This metadata for test");
+        CreatedMetadata response = api.createMetadata(payload);
+        assertEquals("TEST", response.getName());
     }
 
     @Test
-    public void createMetadataTest() {
+    public void addColumn() {
         PushServiceApi api = new PushServiceApi(new ApiClient().setBasePath("http://localhost:" + port));
-        InlineObject payload = new InlineObject();
-        InlineResponse201 response = api.createMetadata(payload);
-        assertEquals(response.getId(), "abcd-efgh");
+        Column payload = new Column();
+        payload.setName("Test");
+        payload.setDataTypeName("number");
+        CreatedColumn response = api.addColumn("abcd-efgh", payload);
+        assertEquals("abcd-efgh", response.getId());
+        assertEquals("number", response.getDataTypeName());
     }
+
+    @Test
+    public void insertData() {
+        PushServiceApi api = new PushServiceApi(new ApiClient().setBasePath("http://localhost:" + port));
+        File payload = new File("src/main/resources/test.csv");
+        InstertedData response = api.insertData("abcd-efgh", payload);
+        assertEquals(Integer.valueOf(10), response.getRecordsNumber());
+        assertEquals("abcd-efgh", response.getId());
+    }
+
 }
